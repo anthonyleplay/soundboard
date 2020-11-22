600ad8f6ba594dd9ff4bf561b21a9539839c09a5
Anthony Le Play <anthony.leplay@gmail.com>
2020-11-22 23:13:34 +0100

    ajout des dossiers de base

---
+++ include/part-footer.php
@@ -0,0 +1,40 @@
+<!-- bouton go to top -->
+<button onclick="topFunction()" id="btnUpPage" title="Go to top">▲</button>
+
+<footer>
+    <div class="container-fluid foot1">
+        <div class="row justify-content-center text-center foot1top">
+            <div class="col-12 foot2 foot2top">
+                <i class="fab fa-facebook fa-2x m-1"></i>
+                <i class="fab fa-instagram fa-2x m-1"></i>
+
+            </div>
+            <div class="col-12 col-lg-6 row ">
+                <div class="col-5 px-0 px-md-2 my-4">
+                    <h6 class="text-uppercase font-weight-bold">A propos</h6>
+                    <hr class="bg-pinkwhite mb-4 mt-0 d-inline-block mx-auto" style="width: 100px;">
+                    <p>Service Client<br>
+                    Mentions Légales</p>
+
+                </div>
+                <div class="col-2 p-0 d-flex align-items-center justify-content-center">
+                    <img  src="..\assets\img\logo200plein.png" width="100%">
+                </div>
+                <div class="col-5 px-0 px-md-2 my-4">
+                    <p class="text-uppercase font-weight-bold h6">Contact</p>
+                    <hr class="bg-pinkwhite mt-0 d-inline-block mx-auto" style="width: 100px;">
+
+                    <p>22 rue des aulnes<br>
+                        76610 Le havre<br>
+                        06 65 45 65 45
+                    </p>
+
+                </div>
+            </div>
+            <div class="col-12 foot2 foot2top">
+                <span>Pension Canine Wenki © 2020 Le Play Anthony, tous droits réservés.</span>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
---
+++ include/part-header.php
@@ -0,0 +1,9 @@
+<header>
+    <div id="img-accueil" class="container-fluid">
+        <div class="row justify-content-lg-start justify-content-center text-center" id="filtre-accueil">
+            <div class="col-12 justify-content-center text-center">
+                <h1 class="typo-flavors" id="titreHeader"><img id="img-logo-title" src="..\assets\img\logo761plein.png"> Pension Canine Wenki</h1>
+            </div>
+        </div>
+    </div>
+</header>
