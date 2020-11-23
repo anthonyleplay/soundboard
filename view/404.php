d014ab21cf40e224b352c788fcbecb6b49152d46
Anthony Le Play <anthony.leplay@gmail.com>
2020-11-23 15:10:04 +0100

    debut du projet

--- view/404.php
+++ view/404.php
@@ -1,23 +1,18 @@
-<?php
-session_start();
-require_once "controler\index-ctrl.php";
-?>
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets\css\style.css" type="text/css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link rel="icon" href="assets\img\logo-128.png">
-    <title>soundboard</title>
+    <link rel="stylesheet" href="..\assets\css\style.css" type="text/css">
+
+    <link rel="icon" href="..\assets\img\logo64plein.png">
+    <title>404 not found</title>
 </head>
 
-
 <body>
-
-
+ 404
 
 
     <!-- JS bootstrap -->
@@ -25,7 +20,7 @@ require_once "controler\index-ctrl.php";
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <!-- mon JS -->
-    <script src="assets\js\script.js"></script>
+    <script src="..\assets\js\script.js"></script>
 </body>
 
 </html>
