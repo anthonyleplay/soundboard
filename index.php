0cd540d4d57473abd71a5057626d0b3ac6b913a4
Anthony Le Play <anthony.leplay@gmail.com>
2020-11-28 15:54:35 +0100

    minime

--- index.php
+++ index.php
@@ -9,19 +9,42 @@ require_once "controler\index-ctrl.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets\css\style.css" type="text/css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <link rel="icon" href="assets\img\logo-128.png">
     <title>soundboard</title>
 </head>
 
 
 <body>
-    <!-- header -->
+    <!-- navbar -->
     <?php
-    include_once 'include\part-header.php';
+    include_once 'include\part-navbar.php';
     ?>
 
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
 
+                </div>
+                <div class="col-3">
+
+                </div>
+                <div class="col-3">
+
+                </div>
+                <div class="col-3">
+
+                </div>
+                <div class="col-3">
+
+                </div>
+                <div class="col-3">
+
+                </div>
+            </div>
+        </div>
+    </main>
 
     <!-- footer -->
     <?php
@@ -31,7 +54,7 @@ require_once "controler\index-ctrl.php";
     <!-- JS bootstrap -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"></script>
     <!-- mon JS -->
     <script src="assets\js\script.js"></script>
 </body>
