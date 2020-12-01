e7bc4ca35ea69f5d36e8910b73374592c366e01c
Anthony Le Play <anthony.leplay@gmail.com>
2020-12-01 17:51:21 +0100

    modif navbar

--- include/part-navbar.php
+++ include/part-navbar.php
@@ -1,4 +1,30 @@
-<header>
-
-
-</header>
+<nav class="navbar navbar-expand-lg navbar-dark bg-green">
+    <div class="navbar-collapse collapse w-100 dual-collapse2 order-1 order-md-0">
+        <ul class="navbar-nav ml-auto text-center">
+            <li class="nav-item active">
+                <a class="nav-link bg-yellow" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link bg-yellow" href="#">Link</a>
+            </li>
+        </ul>
+    </div>
+    <div class="mx-auto my-2 order-0 order-md-1 position-relative">
+        <a class="mx-auto" href="#">
+            <img id="logo" src="assets/img/logo1.jpg" class="rounded-circle">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="navbar-collapse collapse w-100 dual-collapse2 order-2 order-md-2">
+        <ul class="navbar-nav mr-auto text-center">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+    </div>
+</nav>
